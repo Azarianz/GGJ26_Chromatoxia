f24352f7cb16d7460fde528495387067a7efc3f2
Azarianz <azarianz29@gmail.com>
2026-01-30 22:20:20 +0800

    Player Script

---
+++ Source/GGJ26_Chromatoxia/Assets/_Main/Source/Core/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //PlayerStats
+    int currentHP, maxHP = 100;
+    int currentArmorHP, currentArmorMaxHP = 50;
+    public float moveSpeed = 5.0f;
+
+    //Player Oxygen (Update Slider)
+    float currentOxygen, maxOxygen;
+    float currentToxin, maxToxin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void InputHandler()
+    {
+        
+    }
+}
